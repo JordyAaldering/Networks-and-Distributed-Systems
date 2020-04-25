2603eec7dc4ced276f1971ad19e9440f1b79180d
JordyAaldering <aalderingjordy@gmail.com>
2020-04-25 14:54:15 +0200

    Update test.py

--- src/test.py
+++ src/test.py
@@ -36,11 +36,10 @@ class TestFramework(unittest.TestCase):
     def setUp(self):
         """ Prepare for testing. """
         run_command(NETEM_ADD)
+        self.process = Popen(["python3", "./server.py"])
 
         with open("file.in", "rb") as f:
             self.content = f.read()
-        
-        self.process = Popen(["python3", "./server.py"])
 
     def tearDown(self):
         """ Clean up after testing. """
@@ -48,42 +47,36 @@ class TestFramework(unittest.TestCase):
         self.process.kill()
 
     def test_ideal_network(self):
-        """ Reliability over an ideal src. """
+        """ Reliability over an ideal network. """
         self._test_client()
 
     def test_flipping_network(self):
         """ Reliability over network with bit flips which sometimes results in lower layer packet loss. """
-        # Setup environment.
         run_command(NETEM_CHANGE.format("corrupt 1%"))
         self._test_client()
 
     def test_duplicates_network(self):
         """ Reliability over network with duplicate packets. """
-        # Setup environment.
         run_command(NETEM_CHANGE.format("duplicate 10%"))
         self._test_client()
 
     def test_lossy_network(self):
         """ Reliability over network with packet loss. """
-        # Setup environment.
         run_command(NETEM_CHANGE.format("loss 10% 25%"))
         self._test_client()
 
     def test_reordering_network(self):
         """ Reliability over network with packet reordering. """
-        # Setup environment.
         run_command(NETEM_CHANGE.format("delay 20ms reorder 25% 50%"))
         self._test_client()
 
     def test_delayed_network(self):
         """ Reliability over network with delay relative to the timeout value. """
-        # Setup environment.
         run_command(NETEM_CHANGE.format(f"delay {str(timeout)}ms 20ms"))
         self._test_client()
 
     def test_all_bad_network(self):
         """ Reliability over network with all of the above problems. """
-        # Setup environment.
         run_command(NETEM_CHANGE.format("corrupt 1% duplicate 10% loss 10% 25% delay 20ms reorder 25% 50%"))
         self._test_client()
 
@@ -96,12 +89,10 @@ class TestFramework(unittest.TestCase):
         finally:
             client.close()
 
-        # Content received by server matches the content sent by client.
         with open("file.out", "rb") as f:
             assert f.read() == self.content
 
 if __name__ == "__main__":
-    # Parse command line arguments.
     parser = ArgumentParser(description="bTCP tests")
     parser.add_argument("-w", "--window", help="Define bTCP window size used", type=int, default=window)
     parser.add_argument("-t", "--timeout", help="Define the timeout value used (ms)", type=int, default=timeout)
@@ -110,8 +101,5 @@ if __name__ == "__main__":
     window = args.window
     timeout = args.timeout
 
-    # Pass the extra arguments to unittest.
     sys.argv[1:] = extra
-
-    # Start test suite.
     unittest.main()
